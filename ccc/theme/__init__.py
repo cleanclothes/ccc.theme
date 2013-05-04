ade1e6552b25ecbcfcd100ee3b2365c506825255
Michael Davis <m.r.davis@me.com>
2013-05-04 12:01:53 +0100

    Add a skins folder and the favicon

--- ccc/theme/__init__.py
+++ ccc/theme/__init__.py
@@ -1,5 +1,6 @@
-# -*- extra stuff goes here -*-
+from Products.CMFCore.DirectoryView import registerDirectory
 
+registerDirectory('skins', globals())
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
